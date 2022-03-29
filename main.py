5480908542f125305a5aeb13da0a21d193354592
Pl251251 <91552726+Pl251251@users.noreply.github.com>
2022-03-29 11:45:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,38 +14,47 @@ pts2 = np.float32([[0, 0], [400, 0], [0, 550], [400, 550]])
 matrix = cv2.getPerspectiveTransform(pts1, pts2)
 #warp image
 result = cv2.warpPerspective(img, matrix, (400,550))
+x1= -100
+y1=520
+x2=
+y2=
+x3=
+y3=
+x4=
+y4=
+for x in range(20):
+        pts1 = np.float32([[773, 1968], [2088, 1958], [271, 3257], [2828, 3207]])
+        pts2 = np.float32([[0, 0], [400, 0], [0, 550], [400, 550]])
+        #gray and edge
+        gray = cv2.cvtColor(result,cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(gray,50,150,apertureSize = 3)
 
+        #find lines
+        linesP = cv2.HoughLinesP(edges, 1, np.pi / 180, 15, None, 50, 10)
+        #sort lines
+        c=9000
+        d=0
+        for i in linesP:
+            l = i[0]
+            cv2.line(result, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv2.LINE_AA)
+            if (l[0]<c):
+                line1 =i
+                c= l[0]
+            if (l[0]>d):
+                line2 =i
+                d=l[0]
 
-#gray and edge
-gray = cv2.cvtColor(result,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,50,150,apertureSize = 3)
-
-#find lines
-linesP = cv2.HoughLinesP(edges, 1, np.pi / 180, 15, None, 50, 10)
-#sort lines
-c=9000
-d=0
-for i in linesP:
-    l = i[0]
-    cv2.line(result, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv2.LINE_AA)
-    if (l[0]<c):
-        line1 =i
-        c= l[0]
-    if (l[0]>d):
-        line2 =i
-        d=l[0]
-
-#draw left most and right most lines
-info1 = line1[0]
-info2 =line2[0]
-y1= int((info1[1] +info2[1])/2)
-y2 =int((info1[3] +info2[3])/2)
-pt1 = (int(((info1[0]) +int(info2[0]))/2), y1 )
-pt2 = (int((info1[2] +info2[2])/2), y2)
-if (y1>y2):
-    cv2.arrowedLine(result, pt1, pt2, (255, 0, 0), 1, cv2.LINE_AA)
-elif(y1<y2):
-    cv2.arrowedLine(result, pt2, pt1, (255, 0, 0), 1, cv2.LINE_AA)
+        #draw left most and right most lines
+        info1 = line1[0]
+        info2 =line2[0]
+        y1= int((info1[1] +info2[1])/2)
+        y2 =int((info1[3] +info2[3])/2)
+        pt1 = (int(((info1[0]) +int(info2[0]))/2), y1 )
+        pt2 = (int((info1[2] +info2[2])/2), y2)
+        if (y1>y2):
+            cv2.arrowedLine(result, pt1, pt2, (255, 0, 0), 1, cv2.LINE_AA)
+        elif(y1<y2):
+            cv2.arrowedLine(result, pt2, pt1, (255, 0, 0), 1, cv2.LINE_AA)
 #tranform image back
 matrix2 = cv2.getPerspectiveTransform(pts2, pts1)
 final = cv2.warpPerspective(result, matrix2, (width, height))
